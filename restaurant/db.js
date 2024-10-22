a695180959b17c75d5e50795c6948581c92a583e
Vinay Bibyan <vinaybibyan09@gmail.com>
2024-10-23 01:11:32 +0530

    added env files

--- restaurant/db.js
+++ restaurant/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/restaunt'
+require('dotenv').config();
+// const mongoURL = process.env.LOCAL_DB_URL
+const mongoURL = process.env.DB_URL;
 mongoose.connect(mongoURL,{
  // useNewUrlParser: true,
  // useUnifiedTopology: true
--- restaurant/server.js
+++ restaurant/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const menu = require('./model/menu');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
+const PORT = process.env.PORT || 3000;
+
 
 app.get('/', function (req, res) {
     res.send('Welcome to restraunt wesite')
@@ -16,4 +19,4 @@ app.use('/menu',menuRoutes)
 const personRoutes = require('./routes/personRoutes');
 app.use('/person',personRoutes)
  
-app.listen(3000)
+app.listen(PORT);
